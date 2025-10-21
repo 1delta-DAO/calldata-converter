017f6ae08d204a8a76b5fbaed51e2f0e9c08f266
Max <0xmaxyz@gmail.com>
2025-10-21 13:09:02 +0330

    add compound and silo util functions

--- src/consts.ts
+++ src/consts.ts
@@ -1,13 +1,21 @@
 import path from "path"
 
-export const BASE_REPO_URL = "https://github.com/1delta-DAO/contracts-delegation/raw/refs/heads/main/"
+const branch = "main"
+export const BASE_REPO_URL = `https://github.com/1delta-DAO/contracts-delegation/raw/refs/heads/${branch}/`
 export const CALLDATA_LIB_PATH = "test/composer/utils/CalldataLib.sol"
 export const CALLDATA_LIB_URL = `${BASE_REPO_URL}${CALLDATA_LIB_PATH}`
 
 export const DATA_DIR = "./data"
 export const INPUT_DIR = path.join(DATA_DIR, "input")
 export const OUTPUT_DIR = path.join(DATA_DIR, "output")
-export const HARDCODED_FUNCTIONS = ["generateAmountBitmap", "getMorphoCollateral", "getMorphoLoanAsset", "rightPadZero"]
+export const HARDCODED_FUNCTIONS = [
+    "generateAmountBitmap",
+    "getMorphoCollateral",
+    "getMorphoLoanAsset",
+    "rightPadZero",
+    "encodeCompoundV2SelectorId",
+    "encodeSiloV2CollateralMode",
+]
 
 export const LIB_NAME = path.basename(CALLDATA_LIB_PATH, ".sol")
 
--- src/conv.ts
+++ src/conv.ts
@@ -313,7 +313,7 @@ export function convertToTS(solidityCode: string, debug: boolean = false): Conve
 
     output += `
   import { type Hex, type Address, zeroAddress } from "viem";
-  import { encodePacked, uint128, uint8, uint112, uint16, uint256, _PRE_PARAM, _SHARES_MASK, _UNSAFE_AMOUNT, generateAmountBitmap, newbytes, bytes, getMorphoCollateral, getMorphoLoanAsset, rightPadZero } from "../../src/utils.ts";
+  import { encodePacked, uint128, uint8, uint112, uint16, uint256, _PRE_PARAM, _SHARES_MASK, _UNSAFE_AMOUNT, generateAmountBitmap, newbytes, bytes, getMorphoCollateral, getMorphoLoanAsset, rightPadZero, encodeCompoundV2SelectorId, encodeSiloV2CollateralMode } from "../../src/utils.ts";
   `
 
     // Add enum definitions
